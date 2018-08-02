4b62d67acb363ef677623d107c47274203a97422
lumengbit1 <33515760+lumengbit1@users.noreply.github.com>
2018-08-02 23:09:24 +1000

    v1.3增加编辑客户和删除客户

--- routes/edit.js
+++ routes/edit.js
@@ -14,20 +14,31 @@ router.post('/', function(req, res, next) {
 
            if(typeof(req.body.delete)!='undefined'){
                 //console.log('delete',req.body.data)
-                querydata= 'DELETE FROM product.producttable WHERE id='+ req.body.data;
-                conn.query(querydata, [], function(err,result) {
-                    if (err) {
-                        return next(err);
-                    }else{
-                        res.send(JSON.stringify(result));
-                    }
-                })
+                querydata= 'DELETE FROM product.custom WHERE id='+ req.body.data;
+                queryproduct = 'DELETE FROM product.product WHERE customname=(SELECT `name` FROM product.custom where id='+req.body.data+')'
+                //console.log('querydata',querydata)
+               //console.log('queryproduct',queryproduct)
+               conn.query(queryproduct, [], function(err,result) {
+                   if (err) {
+                       return next(err);
+                       console.log('failed')
+                   }else{
+                       console.log('success')
+                       conn.query(querydata, [], function(err,result) {
+                           if (err) {
+                               return next(err);
+                           }else{
+                               res.send(JSON.stringify(result));
+                           }
+                       })
+                   }
+               })
+
             }else if(typeof(req.body.edit)!='undefined'){
-               //console.log('edit',Object.values(req.body)[2])
-                querydata= 'UPDATE product.producttable SET name=\''+ Object.values(req.body)[2]+'\',norm='+Object.values(req.body)[3]+',number='+Object.values(req.body)[4]
-                +',price='+Object.values(req.body)[5]+',memberprice='+Object.values(req.body)[6]+',pay='+Object.values(req.body)[7]+',address='+Object.values(req.body)[8]
-                +',code='+Object.values(req.body)[9]+',remark='+Object.values(req.body)[10]+' WHERE id='+Object.values(req.body)[1];
-               //console.log('edit',querydata)
+               console.log('edit',Object.values(req.body))
+                querydata= 'UPDATE product.custom SET name=\''+ Object.values(req.body)[1]+'\',member='+Object.values(req.body)[2]+',address='+Object.values(req.body)[3]
+                +',ordernum='+Object.values(req.body)[4]+',remark='+Object.values(req.body)[5]+' WHERE id='+Object.values(req.body)[6];
+
                conn.query(querydata, [], function(err,result) {
                    if (err) {
                        return next(err);
